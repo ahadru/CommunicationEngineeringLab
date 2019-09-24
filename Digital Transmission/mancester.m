b133998d6d5b16c950440f0c43e2ae6d543ac305
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2019-09-24 20:21:37 +0600

    mancester done

--- Digital Transmission/mancester.m
+++ Digital Transmission/mancester.m
@@ -1,21 +1,36 @@
-function [t,x] = manchester(bits, bitrate)
-% MANCHESTER Encode bit string using Manchester code.
-%   [T, X] = MANCHESTER(BITS, BITRATE) encodes BITS array using Manchester
-%   code with given BITRATE. Outputs are time T and encoded signal
-%   values X.
-% Copyright (c) 2013 Yuriy Skalko <yuriy.skalko@gmail.com>
-T = length(bits)/bitrate; % full time of bit sequence
+input_bits = [1 0 1 1 1 0 0 1 0 0 1];
+subplot(311);
+stem(input_bits,'c','LineWidth',1);
+title('Inputs');
+
+T = length(input_bits)/bitrate; % full time of bit sequence
 n = 200;
-N = n*length(bits);
+N = n*length(input_bits);
 dt = T/N;
 t = 0:dt:T;
-x = zeros(1,length(t)); % output signal
-for i = 0:length(bits)-1
-  if bits(i+1) == 1
-    x(i*n+1:(i+0.5)*n) = 1;
-    x((i+0.5)*n+1:(i+1)*n) = -1;
+signal = zeros(1,length(t)); % output signal
+for i = 0:length(input_bits)-1
+  if input_bits(i+1) == 1
+    signal(i*n+1:(i+0.5)*n) = 1;
+    signal((i+0.5)*n+1:(i+1)*n) = -1;
   else
-    x(i*n+1:(i+0.5)*n) = -1;
-    x((i+0.5)*n+1:(i+1)*n) = 1;
+    signal(i*n+1:(i+0.5)*n) = -1;
+    signal((i+0.5)*n+1:(i+1)*n) = 1;
   end
 end
+
+subplot(312);
+plot(t,signal,'g','LineWidth',2);
+title('Signal');
+  
+%demodulation
+output_bits = zeros(1,length(input_bits));
+for i = 0:length(input_bits)-1
+    if signal(i*n+1) == 1
+        output_bits(i+1) = 1;
+    end        
+end 
+
+subplot(313);
+stem(output_bits,'b','LineWidth',1);
+title('Outputs');  
