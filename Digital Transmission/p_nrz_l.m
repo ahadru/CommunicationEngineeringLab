d6f65577db20fde6eeb836f7c92c07d1b4c2c424
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2019-09-24 16:14:14 +0600

    Polar NRZ-L line coding done

---
+++ Digital Transmission/p_nrz_l.m
@@ -0,0 +1,34 @@
+input_bits = [1 0 1 1 1 0 0 1 0 0 1];
+subplot(311);
+stem(input_bits,'c','LineWidth',1);
+title('Inputs');
+
+bitrate = 2;
+T = length(input_bits)/bitrate;
+n = 200;
+N = n*length(input_bits);
+dt = T/N;
+t = 0:dt:T;
+signal = zeros(1,length(t));
+ 
+for i = 0:length(input_bits)-1
+    if input_bits(i+1) == 1
+        signal(i*n+1:(i+1)*n) = 1;
+    else
+        signal(i*n+1:(i+1)*n) = -1;
+    end
+end
+
+subplot(312);
+plot(t,signal,'g','LineWidth',2);
+title('Signal');
+  
+%demodulation
+output_bits = zeros(1,length(bits));
+for i = 0:length(input_bits)-1
+    output_bits(i+1) = signal(i*n+1);
+end 
+
+subplot(313);
+stem(input_bits,'b','LineWidth',1);
+title('Outputs');  
