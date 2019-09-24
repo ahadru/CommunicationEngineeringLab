fbdfba247a627df5207fea3f2a3c72c6fa7b0390
Sheikh Ahaduzzaman <ahadsheikh1999@gmail.com>
2019-09-25 00:10:16 +0600

    Finish

---
+++ Analog To Analog Transmission/am_.m
@@ -0,0 +1,36 @@
+%Ac=input('enter carrier signal amplitude');
+%Am=input('enter message signal amplitude');
+%fc=input('enter carrier frequency');
+%fm=input('enter message frequency');% fm<fc
+%m=input('enter modulation index');
+%t=input('enter time period');
+Ac =  1;
+Am = 1;
+fc = 30;
+fm = 10;
+m = 1;
+t = 1;
+
+t1=linspace(0,t,1000);
+y1=sin(2*pi*fm*t1 + pi/4) + 0.5*cos(2*pi*fm*t1 + (3*pi)/4); % message signal
+y2=sin(2*pi*fc*t1); % carrier signal
+
+eq=(m*y1).*(Ac.*y2);
+subplot(311);
+plot(t1,y1);
+xlabel('Time');
+ylabel('Amplitude');
+title('Message signal')
+
+subplot(312)
+plot(t1,y2);
+xlabel('Time');
+ylabel('Amplitude');
+title('Carrier signal');
+
+subplot(313);
+%plot(t1,eq);
+plot(t1,eq,'r');
+xlabel('Time');
+ylabel('Amplitude');
+title('Modulated signal');
--- Digital Transmission/tmp.m
+++ Digital Transmission/tmp.m
@@ -1,2 +1,94 @@
-bi = [1 2 3 4]
-disp(bi(1))
+clear;
+
+bits = [0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1,0];
+
+bit_rate=1;
+voltage=5;
+tmp = voltage;
+sign = -1;
+mrk = 1;
+voltage = sign*voltage;
+cn0=0;
+cn1=0;
+in = 0;
+
+for i = 1:length(bits)
+    
+    if bits(i) == 0
+        cn0 = cn0+1;
+    else
+        cn1=cn1+1;
+        cn0=0;
+    end
+    
+    if cn0>7
+        y_level(i-4) = voltage;
+        y_level(i-3) = -voltage;
+        y_level(i-1) = -voltage;
+        y_level(i) = voltage;
+        cn0=0;
+        c1=0;
+    elseif bits(i)==0
+        y_level(i) = 0;
+    else 
+        y_level(i) = -voltage;
+        voltage=y_level(i);
+    end
+end
+
+voltage=tmp;
+Time=length(bits)/bit_rate; 
+frequency = 1000;
+time = 0:.01:Time;
+x = 1;
+
+for i = 1:length(time)
+    y_value(i)= y_level(x);
+    if time(i)*bit_rate>=x
+        x= x+1;
+    end
+end
+     
+plot(time,y_value);
+axis([0 Time -voltage-2 voltage+2]);
+
+
+% demodulation
+
+
+
+% demodulation
+
+i=1;
+in=1;
+tmp=sign;
+mr = 1;
+in1 = -1;
+
+for j=1:length(time)
+  dm(i) = y_value(j)/voltage;
+  if time(j)*bit_rate>=i 
+      if dm(i)==0
+        ans_bits(in)=0;
+      else
+        if in~=in1+1
+            ans_bits(in)=1;
+        end
+        if tmp== dm(i)&&i>8;
+            in1 = in;
+            ans_bits(in) = 0;
+            ans_bits(in+1) = 0;
+        end
+      end 
+      
+      if dm(i)~=0
+        tmp = dm(i);
+      end
+      i=i+1;
+      in= in+1;
+  end
+end
+ 
+
+disp('Demodulation : ')
+disp(ans_bits)
